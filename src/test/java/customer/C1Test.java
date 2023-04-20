bd1a547b36e87a4ac7c4f8aeee9e7242fc29a86f
vishalct007 <vishalct007@gmail.com>
2023-04-20 16:05:58 +0530

    first commit

---
+++ src/test/java/customer/C1Test.java
@@ -0,0 +1,24 @@
+package customer;
+
+import org.testng.annotations.Test;
+
+public class C1Test {
+	
+	@Test
+	public void t1() {
+		System.out.println("t1 method of c1 class");
+		
+	}
+	
+	
+	@Test
+	public void t2() {
+		System.out.println("t2 method of c1 class");
+		
+		
+		
+		
+	}
+	
+
+}
---
+++ src/test/java/customer/C2Test.java
@@ -0,0 +1,23 @@
+package customer;
+
+import org.testng.annotations.Test;
+
+public class C2Test {
+	
+	
+	@Test
+	public void t3() {
+		System.out.println("t3 method of c2 class");
+		
+	}
+	
+	@Test
+	public void t4() {
+		System.out.println("t3 method of c2 class");
+		
+		
+		
+		
+	}
+
+}
